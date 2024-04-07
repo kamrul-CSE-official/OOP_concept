c5c5c4ad4f9d7af721bb8e70568bf193dcfef5c9
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-04-07 16:35:26 +0600

    update

---
+++ chapter4/problme10.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import webbrowser as wb
+
+url = "http://dimikcomputing.com"
+response = requests.get(url)
+
+with open("dimik.html", "w", encoding=response.encoding) as f: 
+    f.write(response.text)
+
+file_path = os.path.realpath("dimik.html")
+print(file_path)
+wb.open("file://"+file_path)
