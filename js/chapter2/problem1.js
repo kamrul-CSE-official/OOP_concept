f8afb740ccff4cd8301aa749cf76d9ef74dc2d7a
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-04-13 09:10:14 +0600

    Call and Apply method and this key word

--- js/chapter2/problem1.js
+++ js/chapter2/problem1.js
@@ -1,3 +1,7 @@
+// -------------
+// => this key word
+// -------------
+
 console.log(this);
 // console.log(this === window);
 
@@ -10,6 +14,10 @@ console.log(name, address);
 
 // const name = "Kamrul";
 // console.log(name);
+
+// -------------
+// 1. Call method
+// -------------
 
 var myCustomObj = {
   name: "MD.Kamrul Hasan",
@@ -25,3 +33,43 @@ var myCustomObj = {
 
 myCustomObj.anotherObj.value();
 myCustomObj.anotherObj.value.call(myCustomObj);
+
+var karim = {
+  name: "MD.Kamrul Hasan",
+  dob: 1996,
+  age: function (currentYear) {
+    console.log(this.name + " is " + (currentYear - this.dob) + " years old!");
+  },
+};
+
+karim.age(2024);
+
+var karim = {
+  name: "MD.Kamrul Hasan",
+  dob: 1996,
+  age: function (currentYear) {
+    console.log(this.name + " is " + (currentYear - karim.dob) + " years old!");
+  },
+};
+
+karim.age(2024);
+
+// -------------
+// 2. apply method
+// -------------
+
+var myCustomObj = {
+  name: "Zonayed Ahmed",
+  age: 21,
+  job: "Student",
+  anotherObj: {
+    name: "Ahmed Zonayed",
+    value: function () {
+      console.log("My name is " + this.name);
+    },
+  },
+};
+myCustomObj.anotherObj.value.apply(myCustomObj);
+myCustomObj.anotherObj.value();
+
+
